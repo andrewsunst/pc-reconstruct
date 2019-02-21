eed7ec14ada2bb057f4cc27e844220043bc728e4
LAPTOP-GMKQT98D\Andrew <suns3@students.rowan.edu>
2019-02-21 16:14:58 -0500

    add cuda

--- train.py
+++ train.py
@@ -12,6 +12,20 @@ import torch.nn.functional as F
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
-sys.path.append(os.path.join((BASE_DIR, 'models')))
+sys.path.append(os.path.join(BASE_DIR, 'models'))
 
+parser = argparse.ArgumentParser(description='PyTorch Point Cloud Classification Model')
+parser.add_argument('--cuda', type=str, default='false', help='use CUDA')
+
+args = parser.parse_args()
+
+args.device = None
+if args.cuda=='true':
+    if torch.cuda.is_available():
+        args.device = torch.device('cuda')
+    else:
+        print("cuda not available")
+else:
+    args.device = torch.device('cpu')
+print(args.device)
 
