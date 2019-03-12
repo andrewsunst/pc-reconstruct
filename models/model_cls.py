d4f4daba903d0fb77b82b56be0f32e456b21cd03
Andrew Sun <andrew_sunshengtao@hotmail.com>
2019-03-12 12:33:03 -0400

    conv update

--- models/model_cls.py
+++ models/model_cls.py
@@ -1,8 +1,42 @@
 import torch.nn as nn
 import numpy as np
+import torch.nn.functional as F
+import  torch
 import sys
 import os
 import math
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
-sys.path.append(os.path.join(BASE_DIR, '../utils'))
+
+
+def input_transform_net(point_cloud, K=3):
+
+    batch_size = point_cloud.get_shape()[0].value
+    num_point = point_cloud.get_shape()[1].value
+    input_image = point_cloud[:, :, :, None]
+
+
+def _variable_with_weight_decay(shape, stddev, wd, use_xavier=True):
+
+
+
+def conv2d(inputs,
+           num_output_channels,
+           kernel_size,
+           scope,
+           stride=[1, 1],
+           padding='SAME',
+           use_xavier=True,
+           stddev=1e-3,
+           weight_decay=0.0,
+           activation_fn=F.relu,
+           bn=False,
+           bn_decay=None,
+           is_training=None):
+    kernel_h, kernel_w = kernel_size
+    num_in_channels = inputs.get_shape()[-1].value
+    kernel_shape = [kernel_h, kernel_w,
+                    num_in_channels, num_output_channels]
+
+
+
