5ec00f8509149af22272fb14a8bd2eff2d95809a
andrewsunst <andrew_sunshengtao@hotmail.com>
2019-03-12 15:59:39 -0400

    bug class fix

--- models/model_cls.py
+++ models/model_cls.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
 
 
-def input_transform_net(nn.Module):
+class input_transform_net(nn.Module):
     def __init__(self):
         super(input_transform_net, self).__init__()
         self.conv1 = torch.nn.Conv2d(1, 64, (1,3), stride=(1, 1))
@@ -37,29 +37,4 @@ def input_transform_net(nn.Module):
 
 
 def _variable_with_weight_decay(shape, stddev, wd, use_xavier=True):
-
-
-
-
-
-def conv2d(inputs,
-           num_output_channels,
-           kernel_size,
-           scope,
-           stride=[1, 1],
-           padding='SAME',
-           use_xavier=True,
-           stddev=1e-3,
-           weight_decay=0.0,
-           activation_fn=F.relu,
-           bn=False,
-           bn_decay=None,
-           is_training=None):
-    kernel_h, kernel_w = kernel_size
-    num_in_channels = inputs.get_shape()[-1].value
-    kernel_shape = [kernel_h, kernel_w,
-                    num_in_channels, num_output_channels]
-
-
-
-
+    return 0
